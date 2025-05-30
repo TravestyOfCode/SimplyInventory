f559644f83373bb0084da349ddedd1f659019418
travis@travestyofcode.com <travis@travestyofcode.com>
2025-05-30 04:21:07 -0400

    Adds static configuration extension for service collection.

---
+++ SimplyInventory.Data/Serivces/ConfigurationService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimplyInventory.Data.Serivces;
+
+public static class ConfigurationService
+{
+    public static IServiceCollection AddDataServices(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<AppDbContext>(c => c.UseSqlServer(connectionString))
+            .AddDefaultIdentity<AppUser>(c => c.SignIn.RequireConfirmedAccount = true)
+            .AddRoleManager<IdentityRole>()
+            .AddDefaultTokenProviders()
+            .AddEntityFrameworkStores<AppDbContext>();
+
+        services.AddMediatR(c => c.RegisterServicesFromAssembly(System.Reflection.Assembly.GetExecutingAssembly()));
+
+        return services;
+    }
+}
