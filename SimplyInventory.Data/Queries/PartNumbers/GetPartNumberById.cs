05e00c39472968727b42acffc0d0347363b69674
travis@travestyofcode.com <travis@travestyofcode.com>
2025-05-30 05:16:20 -0400

    Adds PartNumber queries.

---
+++ SimplyInventory.Data/Queries/PartNumbers/GetPartNumberById.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimplyInventory.Data.Models;
+
+namespace SimplyInventory.Data.Queries.PartNumbers;
+
+public class GetPartNumberById : IRequest<Result<PartNumber>>
+{
+    public int Id { get; set; }
+}
+
+internal class GetPartNumberByIdHandler(AppDbContext dbContext, ILogger<GetPartNumberByIdHandler> logger)
+    : IRequestHandler<GetPartNumberById, Result<PartNumber>>
+{
+    public async Task<Result<PartNumber>> Handle(GetPartNumberById request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var model = await dbContext.PartNumbers
+                .Where(p => p.Id.Equals(request.Id))
+                .ProjectToModel()
+                .SingleOrDefaultAsync(cancellationToken);
+
+            if (model == null)
+            {
+                return Error.NotFound();
+            }
+
+            return model;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unexpected error handling request: {request}", request);
+            return Error.ServerError();
+        }
+    }
+}
---
+++ SimplyInventory.Data/Queries/PartNumbers/GetPartNumbersByItemId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimplyInventory.Data.Models;
+
+namespace SimplyInventory.Data.Queries.PartNumbers;
+
+public class GetPartNumbersByItemId : IRequest<Result<List<PartNumber>>>
+{
+    public int ItemId { get; set; }
+}
+
+internal class GetPartNumbersByItemIdHandler(AppDbContext dbContext, ILogger<GetPartNumberByIdHandler> logger)
+    : IRequestHandler<GetPartNumbersByItemId, Result<List<PartNumber>>>
+{
+    public async Task<Result<List<PartNumber>>> Handle(GetPartNumbersByItemId request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await dbContext.PartNumbers
+                .Where(p => p.ItemId.Equals(request.ItemId))
+                .ProjectToModel()
+                .ToListAsync(cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unexpected error handling request: {request}", request);
+            return Error.ServerError();
+        }
+    }
+}
