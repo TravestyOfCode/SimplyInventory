d8daed56ee85894ab8d5babad3fdb81531d1616d
travis@travestyofcode.com <travis@travestyofcode.com>
2025-05-30 04:16:26 -0400

    Adds base AppUser and DbContext.

---
+++ SimplyInventory.Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimplyInventory.Data;
+
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+    }
+}
+
+
