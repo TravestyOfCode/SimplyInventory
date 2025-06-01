f9b9148c12558a4abfbf687dd80db14ea2d8b92c
travis@travestyofcode.com <travis@travestyofcode.com>
2025-06-01 06:35:41 -0400

    Switches to use primary constructor.

--- SimplyInventory.Data/AppDbContext.cs
+++ SimplyInventory.Data/AppDbContext.cs
@@ -6,15 +6,11 @@ using SimplyInventory.Data.Entity;
 
 namespace SimplyInventory.Data;
 
-internal class AppDbContext : IdentityDbContext<AppUser>
+internal class AppDbContext(DbContextOptions options) : IdentityDbContext<AppUser>(options)
 {
     public DbSet<Item> Items { get; set; }
     public DbSet<PartNumber> PartNumbers { get; set; }
     public DbSet<Vendor> Vendors { get; set; }
-
-    public AppDbContext(DbContextOptions options) : base(options)
-    {
-    }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
