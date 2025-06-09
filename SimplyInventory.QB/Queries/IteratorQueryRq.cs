8103878d02085b9abcf2dddfd6e0c0ba66abf740
travis@travestyofcode.com <travis@travestyofcode.com>
2025-06-09 04:45:18 -0400

    Adds base query types.

---
+++ SimplyInventory.QB/Queries/IteratorQueryRq.cs
@@ -0,0 +1,31 @@
+﻿namespace SimplyInventory.QB.Queries;
+
+public abstract class IteratorQueryRq<T> : QueryRq<T>
+{
+    protected Iterator? iterator;
+
+    protected string? iteratorID;
+
+    protected int? iteratorRemainingCount;
+    public int? RemainingCount => iteratorRemainingCount;
+
+    private int? maxReturned;
+    public int? MaxReturned
+    {
+        get => maxReturned;
+        set
+        {
+            if (value == null)
+            {
+                iterator = null;
+                iteratorID = null;
+            }
+            else
+            {
+                iterator = Iterator.Start;
+                iteratorID = null;
+            }
+            maxReturned = value;
+        }
+    }
+}
---
+++ SimplyInventory.QB/Queries/QueryRq.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml.Linq;
+
+namespace SimplyInventory.QB.Queries;
+
+public abstract class QueryRq<T>
+{
+    protected string? requestID = Random.Shared.Next(100, 99999).ToString();
+    public string? RequestID => requestID;
+
+    protected MetaData? metaData;
+    public MetaData? MetaData { get => metaData; set => metaData = value; }
+
+    protected int? statusCode;
+    public int? StatusCode => statusCode;
+
+    protected StatusSeverity? statusSeverity;
+    public StatusSeverity? StatusSeverity => statusSeverity;
+
+    protected string? statusMessage;
+    public string? StatusMessage => statusMessage;
+
+    protected int? retCount;
+    public int? ReturnedCount => retCount;
+
+    public List<string>? IncludeRetElement { get; set; }
+    public List<string>? OwnerID { get; set; }
+
+    protected List<T>? results;
+    public List<T>? Results => results;
+
+    public abstract void ParseResponse(XDocument doc);
+}
